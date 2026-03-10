77274d537b34a978b166ca5a8d0c83fcb7c22169
AshishS444 <ashishsahoo2213@gmail.com>
2026-03-10 15:00:01 +0530

    Credit Paused

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -45,7 +45,7 @@ const Credits = () => {
 
   return (
     <div className='max-w-7xl h-screen overflow-y-scroll mx-auto px-4 sm:px-6 lg:px-8 py-12'>
-      <h2 className='text-3xl font-semibold text-center mb-10 xl:mt-30 text-gray-800 dark:text-white'>Credit Plans are Free</h2>
+      <h2 className='text-3xl font-semibold text-center mb-10 xl:mt-30 text-gray-800 dark:text-white'>Unlimited usage enabled</h2>
 
       {/* <div className='flex flex-wrap justify-center gap-8'>{plans.map((plan)=> (
         <div key={plan._id} className={`border border-gray-200 dark:border-purple-700 rounded-lg shadow hover:shadow-lg transition-shadow p-6 min-w-[300px] flex flex-col ${plan._id === "pro" ? "bg-purple-50 dark:bg-purple-900" : "bg-white dark:bg-transparent"}`}>
